a07f00de995829152d0605b672558fec56a56f0f
Molly Riley <mo.riley@gmail.com>
2021-10-04 16:46:57 +0000

    finish lab

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,28 @@
 """Count words in file."""
 
 import sys
-
 input_file = open(sys.argv[1])
 
-# put your code here.
-word_dict = {}
-#lines = open(input_file)
-for line in input_file:
-    for word in line.rstrip().split():
-        word_dict[word] = word_dict.get(word, 0) + 1
-input_file.close()
+def tokenize(in_file):
+    my_list = []
+    for line in in_file:
+        for word in line.rstrip().split():
+            my_list.append(word)
+    return my_list
 
-for key, value in word_dict.items():
-    print(f"{key} {value}")
+def count_words(list_of_word):
+    word_dict = {}
+    for word in list_of_word: 
+        word_dict[word] = word_dict.get(word, 0) + 1
+    return word_dict
+
+def print_dictionary(dict_):
+    for key, value in dict_.items():
+        print(f"{key} {value}")
+    return
+
+word_list = tokenize(input_file)
+word_dict = count_words(word_list)
+print_dictionary(word_dict)
 
 
